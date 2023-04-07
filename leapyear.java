5f806803ea221ca939f52bfbb7d132651ca2e8ff
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2023-04-07 17:37:20 +0530

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
-public class leapyear {
-   public static void main(String[] args){
-      int year;
-      System.out.println("Enter an Year :: ");
-      Scanner sc = new Scanner(System.in);
-      year = sc.nextInt();
-
-      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-         System.out.println("it is a leap year");
-      else
-         System.out.println("it is not a leap year");
-   }
+  class leapyear
+ {
+ 
+    public static void main(String[] args)
+{
+ 
+        int startYear, endYear, i;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Start Year:");
+        startYear = in.nextInt();
+        System.out.print("Enter the End Year:");
+        endYear = in.nextInt();
+        System.out.println("Leap years:");
+       for (i = startYear; i <= endYear; i++) {
+ 
+             
+    if ( ( i % 4==0) && ( i % 100!=0) || ( i % 400==0) ){
+               System.out.println(i);
+            }
+        }
+    }
 }
