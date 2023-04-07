d1d5d58e35382cc6968be09acb2f68e672b4df71
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2023-04-07 12:02:04 +0530

    Update product.java

--- product.java
+++ product.java
@@ -1,10 +1,10 @@
 import java.io.*; 
-public class product
+public class Product
 {
 	String pcode;
 	String pname;
 	int price;
-	product(String pcode_get, String pname_get, int price_get)
+	Product(String pcode_get, String pname_get, int price_get)
 	{
 	pcode = pcode_get;
 	pname = pname_get;
@@ -13,14 +13,14 @@ public class product
 	
 	public static void main(String[] args) 
 	{
-		product p_1 = new product("A123", "TV", 2);
-		product p_2 = new product("B123", "RADIO", 21);
-		product p_3 = new product("C123", "DVD", 15);
+		Product p_1 = new Product("A123", "TV", 2);
+		Product p_2 = new Product("B123", "RADIO", 21);
+		Product p_3 = new Product("C123", "DVD", 15);
 		if (p_1.price <= p_3.price && p_1.price <= p_2.price)
-			System.out.println("Lowest product price : " + p_1.price);
-		if (p_3.price <= p_1.price && p_3.price <= p_2.price)
-			System.out.println("Lowest product price  : " + p_3.price);
+			System.out.println("Lowest product and price : " + p_1.price   +   p_1.pname);
+		if (p_3.price <= p_1.price && p_3.price <= p_2.price) 
+			System.out.println("Lowest product price  : " + p_3.price  +   p_3.pname);
 		if (p_2.price <= p_3.price && p_2.price <= p_1.price)
-			System.out.println("Lowest product price : " + p_2.price);
+			System.out.println("Lowest product and price : " + p_2.price   +   p_2.pname);
 	}
 }
