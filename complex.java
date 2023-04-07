58274f8e21fbb8deae19a9be77767cb3e63ca8d2
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2023-04-07 13:31:55 +0530

    Update complex.java

--- complex.java
+++ complex.java
@@ -1,18 +1,33 @@
-import java.util.Scanner;
-class complex
-{
-public static void main(String arg[])
-{
-Scanner s=new Scanner(System.in);
-int a,b,c,d;
-System.out.println("Enter the fist complex number");
-a=s.nextInt();
-b=s.nextInt();
-System.out.println("The number is: "+a+"+"+b+"i");
-System.out.println("Enter the second complex number");
-c=s.nextInt();
-d=s.nextInt();
-System.out.println("The number is:"+c+""+""+d+"i");
-System.out.println("The Sum is :"+ (a+c) + " + "+ (b+d) +"i");
-}
+public class Complex {
+    int real,img;
+    Complex(int real, int img)
+    {
+        this.real = real;
+        this.img = img;
+    }
+
+    void display()
+    {
+        System.out.println("The Complex numbers is:");
+        System.out.println(this.real+"+"+this.img+"i");
+    }
+
+    void addition(Complex other)
+    {
+        int t1 = this.real+other.real;
+        int t2 =this.img+other.img;
+
+        System.out.println("The sum of complex number is");
+        System.out.println(t1+"+"+t2+"i");
+    }
+
+    public static void main(String args[])
+    {
+        Complex c1 = new Complex(3,4);
+        Complex c2 = new Complex(5, 2);
+        c1.display();
+        c2.display();
+        c1.addition(c2);
+        
+    }
 }
